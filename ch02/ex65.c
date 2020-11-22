481163e6da760f8fd3f3a950931686ac3414dbb4
machern <13zxchen3@gmail.com>
2020-11-22 13:56:13 +0800

    ex65

--- ch02/ex65.c
+++ ch02/ex65.c
@@ -2,12 +2,17 @@
 
 int odd_ones(unsigned x)
 {
-    //todo
-    return 1;
+    x ^= (x >> 16);
+    x ^= (x >> 8);
+    x ^= (x >> 4);
+    x ^= (x >> 2);
+    x ^= (x >> 1);
+    return x & 1;
 }
 
 int main(int args, char *argv[])
 {
-    
+    printf("%d\n", odd_ones(0b111));
+    printf("%d\n", odd_ones(0b1111));
     return 0;
 }
