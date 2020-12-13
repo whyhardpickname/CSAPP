e106c5474b91b2235822c78ba8f1153db986ddbf
whyhardpickname <13zxchen3@gmail.com>
2020-12-13 10:11:46 +0800

    overflow

--- ch02/ex73.c
+++ ch02/ex73.c
@@ -4,7 +4,8 @@ int saturating_add(int x, int y);
 
 int main()
 {
-    
+    printf("%x\n", saturating_add(0x80000000, 0x80000000));
+    printf("%x\n", saturating_add(0x7fffffff, 0x7fffffff));
     return 0;
 }
 
@@ -12,15 +13,16 @@ int saturating_add(int x, int y)
 {
     int TMin = 0x80000000;
     int TMax = 0x7fffffff;
-    
-    int sum = x + y;
-    int wide_minus_1 = (1 << 5) - 1;
-    int xsign = x >> wide_minus_1;
-    int ysign = y >> wide_minus_1;
-    int sumsign = sum >> wide_minus_1;
-    
-    int xxory = xsign ^ ysign;
 
-    
-    return x + y;
+    int wide_minus_1 = (1 << 5) - 1;
+
+    int sum = x + y;
+    int x_sign = x >> wide_minus_1;
+    int y_sign = y >> wide_minus_1;
+    int sum_sign = sum >> wide_minus_1;
+
+    int is_neg_overflow = x_sign & y_sign & ~sum_sign;
+    int is_pos_overflow = ~x_sign & ~y_sign & sum_sign;
+    int overflow = is_neg_overflow | is_pos_overflow;
+    return (is_neg_overflow & TMin) | (is_pos_overflow & TMax) | (~overflow & sum);
 }
